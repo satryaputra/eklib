392e76d912d2f3e93878d96abbd4bfd65105d370
satryaputra <mohammadekasatryaputra@gmail.com>
2024-08-27 17:12:27 +0700

    init project

---
+++ Api/Program.cs
@@ -0,0 +1,26 @@
+using Api.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+var books = new List<Book>
+    {
+        new() {
+            Id = 1,
+            Title = "Atomic Habits",
+            Author = "James Clear",
+            YearPublished = 2018,
+            Pages = 320,
+            IsAvailable = true
+        }
+    };
+
+app.MapGet("/books", () => books);
+
+
+app.Run();
